50c90a3065d3a1b31e9febe50e9dad7bd9cf91a7
bntrtm <git.bntrtm@tjbainter.com>
2026-02-25 17:19:08 -0600

    feat: initial commit as simple ping bot

---
+++ go.mod
@@ -0,0 +1,18 @@
+module github.com/bntrtm/secret-ermine
+
+go 1.25.0
+
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/sentinelb51/revoltgo v0.0.0-20260126203137-ee907eebd2f9
+)
+
+require (
+	github.com/dolthub/maphash v0.1.0 // indirect
+	github.com/goccy/go-json v0.10.5 // indirect
+	github.com/klauspost/compress v1.18.3 // indirect
+	github.com/lxzan/gws v1.8.9 // indirect
+	github.com/oklog/ulid/v2 v2.1.1 // indirect
+	github.com/philhofer/fwd v1.2.0 // indirect
+	github.com/tinylib/msgp v1.6.3 // indirect
+)
---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/joho/godotenv"
+	// 'sgo' as in "stoat go"
+	sgo "github.com/sentinelb51/revoltgo"
+)
+
+func main() {
+	var err error
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	botToken := os.Getenv("BOT_TOKEN")
+
+	// start a new session
+	session := sgo.New(botToken)
+
+	sgo.AddHandler(session, func(s *sgo.Session, event *sgo.EventReady) {
+		fmt.Printf("Ready to process commands for %d user(s) across %d server(s)\n", len(event.Users), len(event.Servers))
+	})
+
+	sgo.AddHandler(session, func(s *sgo.Session, event *sgo.EventMessage) {
+		handleBotMessage(s, event)
+	})
+
+	err = session.Open()
+	if err != nil {
+		panic(err)
+	}
+
+	// let the bot run by awaiting signals
+	sc := make(chan os.Signal, 1)
+
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+}
+
+func handleBotMessage(session *sgo.Session, msg *sgo.EventMessage) {
+	if msg.Content != "!ping" {
+		return
+	}
+
+	latency := session.WS.Latency()
+	content := latency.String()
+
+	if latency.Milliseconds() == 0 {
+		content = "Still calculating, keep re-trying this command in 15-second intervals."
+	}
+
+	send := sgo.MessageSend{
+		Content: content,
+	}
+
+	message, err := session.ChannelMessageSend(msg.Channel, send)
+	if err != nil {
+		fmt.Println("Error sending message: ", err)
+		return
+	}
+
+	fmt.Println("Sent message:", message.Content)
+}
