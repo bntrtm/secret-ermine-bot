297a5d36eb114051d4c31ea8401719f71c641cb5
bntrtm <git.bntrtm@tjbainter.com>
2026-03-09 21:35:47 -0500

    test: add unit test for finding participant event(s)

--- botstore_test.go
+++ botstore_test.go
@@ -6,6 +6,96 @@ import (
 	// 'sgo' as in "stoat go"
 )
 
+func TestGetParticipantEvent(t *testing.T) {
+	const mockServerPrefix = "TEST"
+	const mockServerID1 = mockServerPrefix + "-EVENT1"
+	const mockServerID2 = mockServerPrefix + "-EVENT2"
+	const outlierServerID = "OUTLIER-EVENT"
+	users := []string{
+		"User1",
+		"User2",
+		"User3",
+		"User4",
+	}
+
+	bot := &botStore{
+		Events: map[string]SecretSantaEvent{
+			mockServerID1: {
+				Participants: map[string]Participant{},
+			},
+			mockServerID2: {
+				Participants: map[string]Participant{},
+			},
+			outlierServerID: {
+				Participants: map[string]Participant{},
+			},
+		},
+		TrackedParticipants: map[string]map[string]struct{}{},
+	}
+	for i, sID := range []string{mockServerID1, mockServerID2, outlierServerID} {
+		sse, ok := bot.Events[sID]
+		if !ok {
+			t.Errorf("could not get event")
+		}
+		// event from mockServerID2 will not include User1
+		// event from outlierServerID will not include User1 nor User2
+		sse.assignParticipants(users[i:])
+		bot.Events[sID] = sse
+
+		err := bot.syncEventParticipants(sID)
+		if err != nil {
+			t.Errorf("could not sync event participants")
+		}
+	}
+
+	t.Run("user not found", func(t *testing.T) {
+		uID := "User0"
+		_, _, err := bot.getParticipantEvent(uID, "")
+		if err == nil {
+			t.Errorf("expected user not found with empty prefix case, but apparently exists")
+		}
+		_, _, err = bot.getParticipantEvent(uID, mockServerPrefix)
+		if err == nil {
+			t.Errorf("expected user not found with case of prefix provided, but %s exists", uID)
+		}
+	})
+
+	t.Run("empty prefix gets all events", func(t *testing.T) {
+		// User4 with an empty prefix ought to yield three IDs
+		_, matches, err := bot.getParticipantEvent(users[3], "")
+		if err == nil || matches != 3 {
+			t.Errorf("expected an error with three matches, but got: err: %t, matches: %d", err == nil, matches)
+		}
+	})
+
+	// User4 with the "TEST-" prefix ought to yield two IDs
+	t.Run("provided prefix w/ >1 results fails", func(t *testing.T) {
+		sID, matches, err := bot.getParticipantEvent(users[3], mockServerPrefix)
+		if err == nil {
+			t.Errorf("expected error, got none")
+		}
+		if matches != 2 {
+			t.Errorf("expected %d results, got %d", 2, matches)
+		}
+		if sID != "" {
+			t.Errorf("expected empty server ID, got %s", sID)
+		}
+	})
+
+	t.Run("provided prefix w/ 1 result gets server ID", func(t *testing.T) {
+		sID, matches, err := bot.getParticipantEvent(users[0], mockServerPrefix)
+		if err != nil {
+			t.Errorf("expected no error, got: %s", err)
+		}
+		if matches != 1 {
+			t.Errorf("expected %d results, got %d", 1, matches)
+		}
+		if sID == "" {
+			t.Errorf("expected non-empty server ID, got empty")
+		}
+	})
+}
+
 func TestSyncTrackedParticipants(t *testing.T) {
 	const mockServerID = "TEST-EVENT"
 	users := []string{
